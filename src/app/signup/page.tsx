06cc46297750ed235f2abca8fac59f6e19922207
malik <malikhax3@gmail.com>
2023-08-01 01:01:20 +0500

    mongoDB connection

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+const SignupPage =()=>{
+    return(
+        <div className="flex">
+            <h1 className="text-center text-white text-2xl">Login</h1>
+        </div>
+    )
+}
+export default SignupPage
---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection=mongoose.connection;
+        connection.on('connected',()=>{
+            console.log('MongpDB connected successfully');
+        })
+        connection.on('error',(err)=>{
+            console.log('Mongoose connection error. please make sure MongoDB is running', err);
+            process.exit();   
+        })
+    } catch (error) {
+        console.log('Something goes Wrong!');
+        console.log(error)
+    }
+}
